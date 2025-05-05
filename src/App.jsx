9ee704a252aeb935f80f14a554bb500b0bcd7e7a
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2025-05-06 00:53:41 +0600

    board and interaction

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState } from "react";
+function Square({ value, onSquareClick }) {
+  return (
+    <button
+      className="bg-white border font-bold border-gray-300 h-12 w-12 m-1 leading-9 text-lg"
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+const Board = ({ value }) => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <>
+      <div className="flex justify-center">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="flex justify-center">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="flex justify-center">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+};
+
+export default Board;
